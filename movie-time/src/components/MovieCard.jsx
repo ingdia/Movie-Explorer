a0cd804b3953ce5cbc23a81ae65d5e806b0cb75e
ingdia <d.ingabire2@alustudent.com>
2025-11-04 12:11:14 +0200

    improving the design of card for responsiveness

--- movie-time/src/components/MovieCard.jsx
+++ movie-time/src/components/MovieCard.jsx
@@ -24,18 +24,18 @@ function MovieCard({ movie, isFav, AddtoFavorite, RemoveFromMyFavorite }) {
   }
 
   return (
-    <div key={movie.id} className=" font-serif  bg-neutral-900 hover:rounded-2xl  hover:p-3 p-2 rounded-xl border-2 border-gray-400 hover:border-green-600">
+    <div key={movie.id} className="  font-serif  bg-neutral-900 hover:rounded-2xl  hover:p-3 p-2 rounded-xl border-2 border-gray-400 hover:border-green-600">
       <Link to={`/movie/${movie.id}`}>
        <div className="relative">
-        <img src={movie.image?.medium} alt={movie.name} className="rounded-b-2xl h-60 w-60" />
+        <img src={movie.image?.medium} alt={movie.name} className="rounded-b-2xl h-60 w-full object-fill" />
   <p className="bg-green-600  absolute  px-1 md:px-4  top-2 left-2 ">
   {movie.status}
 </p>
-
+ <p className="bg-gray-600    absolute  px-1 md:px-4   writing-vertical-rl bottom-16 right-2 ">{movie.language}</p>
         <p className="py-4 ">{movie.name}</p>
        </div>
       </Link>
-      <p className="md:absolute md:rotate-[-20deg] md:ml-30 bg-gray-500  w-20 mb-2  md:px-4  rounded-2xl md:-mt-30 ">{movie.language}</p>
+     
       {button}
     </div>
   );
